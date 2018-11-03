a3b5590c1ce64dd7bd0ac6195e8c472927413fd0
Victor Rocha <vrsarmento@gmail.com>
2018-11-03 02:52:35 -0300

    Challenge-29

---
+++ challenge-29/instructor_resolution/js/challenge-29.js
@@ -0,0 +1,112 @@
+(function(DOM) {
+  'use strict';
+
+  /*
+  Vamos estruturar um pequeno app utilizando módulos.
+  Nosso APP vai ser um cadastro de carros. Vamos fazê-lo por partes.
+  A primeira etapa vai ser o cadastro de veículos, que deverá funcionar da
+  seguinte forma:
+  - No início do arquivo, deverá ter as informações da sua empresa - nome e
+  telefone (já vamos ver como isso vai ser feito)
+  - Ao abrir a tela, ainda não teremos carros cadastrados. Então deverá ter
+  um formulário para cadastro do carro, com os seguintes campos:
+    - Imagem do carro (deverá aceitar uma URL)
+    - Marca / Modelo
+    - Ano
+    - Placa
+    - Cor
+    - e um botão "Cadastrar"
+
+  Logo abaixo do formulário, deverá ter uma tabela que irá mostrar todos os
+  carros cadastrados. Ao clicar no botão de cadastrar, o novo carro deverá
+  aparecer no final da tabela.
+
+  Agora você precisa dar um nome para o seu app. Imagine que ele seja uma
+  empresa que vende carros. Esse nosso app será só um catálogo, por enquanto.
+  Dê um nome para a empresa e um telefone fictício, preechendo essas informações
+  no arquivo company.json que já está criado.
+
+  Essas informações devem ser adicionadas no HTML via Ajax.
+
+  Parte técnica:
+  Separe o nosso módulo de DOM criado nas últimas aulas em
+  um arquivo DOM.js.
+
+  E aqui nesse arquivo, faça a lógica para cadastrar os carros, em um módulo
+  que será nomeado de "app".
+  */
+
+  var app = (function(){
+
+
+    return {
+      init: function init() {
+        this.companyInfo();
+        this.initEvents();
+      },
+
+      initEvents: function initEvents() {
+        DOM('[data-js="form"]').on('submit', this.handleSubmit);
+      },
+
+      handleSubmit: function handleSubmit(e) {
+        e.preventDefault();
+        var $tableBody = DOM('[data-js="table_body"]').get();
+        $tableBody.appendChild(app.createNewCar()); 
+      },
+
+      createNewCar: function createNewCar() {
+        var $fragment = document.createDocumentFragment();
+        var $tr = document.createElement('tr');
+        var $tdImage = document.createElement('td');
+        var $tdModel = document.createElement('td');
+        var $tdYear = document.createElement('td');
+        var $tdLicense = document.createElement('td');
+        var $tdColor = document.createElement('td');
+        var $image = document.createElement('img');
+        
+        $image.setAttribute('src', DOM('[data-js="car_image"]').get().value);
+        
+        $tdModel.textContent = DOM('[data-js="car_model"]').get().value;
+        $tdYear.textContent = DOM('[data-js="car_year"]').get().value;
+        $tdLicense.textContent = DOM('[data-js="car_license"]').get().value;
+        $tdColor.textContent = DOM('[data-js="car_color"]').get().value;
+
+        $tdImage.appendChild($image);
+        $tr.appendChild($tdImage);
+        $tr.appendChild($tdModel);
+        $tr.appendChild($tdYear);
+        $tr.appendChild($tdLicense);
+        $tr.appendChild($tdColor);
+
+        return $fragment.appendChild($tr);
+      },
+
+      companyInfo: function companyInfo() {
+        var ajax = new XMLHttpRequest;
+        ajax.open('GET', 'company.json', true);
+        ajax.send();
+        ajax.addEventListener('readystatechange', this.getCompanyInfo, false); 
+      },
+
+      getCompanyInfo: function getCompanyInfo() {
+        if(!app.isRequestReady.call(this)) //this is ajax
+          return;
+        
+        var data = JSON.parse(this.responseText);
+        var $companyName = DOM('[data-js="company_name"]').get();
+        var $companyPhone = DOM('[data-js="company_phone"]').get();
+        $companyName.textContent = data.name;
+        $companyPhone.textContent = data.phone;
+      },
+
+      isRequestReady: function isRequestReady() {
+        return this.readyState === 4 && this.status === 200; //this is ajax
+      }
+    };
+
+  })();
+
+  app.init();
+
+})(window.DOM);
